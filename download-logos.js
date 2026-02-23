4315800eb5dca34374fe16ae4ec9f5fd5b2ab2e0
Fredrik Beckman <fredrik.beckman69@gmail.com>
2026-02-23 11:00:38 +0100

    chore: auto-sync 2026-02-23 11:00:38

--- download-logos.js
+++ download-logos.js
@@ -5,13 +5,17 @@ const outputDir = path.join(__dirname, 'app', 'public', 'logos');
 
 async function run() {
     const url = `https://polisen.se/siteassets/bilder/polisen_vapen_bla.png`;
-    const res = await fetch(url);
+    const res = await fetch(url, {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+        }
+    });
     if (res.ok) {
         const buffer = await res.arrayBuffer();
         fs.writeFileSync(path.join(outputDir, `polisen.png`), Buffer.from(buffer));
         console.log(`Downloaded polisen.png`);
     } else {
-        console.log(`Failed polisen`);
+        console.log(`Failed polisen with error: ${res.status}`);
     }
 }
 
