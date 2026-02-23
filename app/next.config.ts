f6b6b33e32949e9b80b17d3555db5ac9a2ba15f2
Fredrik Beckman <fredrik.beckman69@gmail.com>
2026-02-23 08:52:29 +0100

    chore: auto-sync 2026-02-23 08:52:28

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/Beredskapsplan",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
