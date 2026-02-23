2f88c6a1d4cfe06b9077008d70b872b4c44eb2b0
Fredrik Beckman <fredrik.beckman69@gmail.com>
2026-02-23 10:56:52 +0100

    chore: auto-sync 2026-02-23 10:56:52

--- download-logos.js
+++ download-logos.js
@@ -2,17 +2,14 @@ const fs = require('fs');
 const path = require('path');
 
 const outputDir = path.join(__dirname, 'app', 'public', 'logos');
-if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir, { recursive: true });
-}
 
 async function run() {
-    const url = `https://upload.wikimedia.org/wikipedia/commons/e/ea/Polisen_vapen.svg`;
+    const url = `https://polisen.se/siteassets/bilder/polisen_vapen_bla.png`;
     const res = await fetch(url);
     if (res.ok) {
-        const text = await res.text();
-        fs.writeFileSync(path.join(outputDir, `polisen.svg`), text);
-        console.log(`Downloaded polisen.svg`);
+        const buffer = await res.arrayBuffer();
+        fs.writeFileSync(path.join(outputDir, `polisen.png`), Buffer.from(buffer));
+        console.log(`Downloaded polisen.png`);
     } else {
         console.log(`Failed polisen`);
     }
