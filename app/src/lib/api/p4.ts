c40620b410da60d1a6c8acd6a4d60d0d063e69cf
Fredrik Beckman <fredrik.beckman69@gmail.com>
2026-02-25 08:16:23 +0100

    chore: auto-sync 2026-02-25 08:16:23

--- app/src/lib/api/p4.ts
+++ app/src/lib/api/p4.ts
@@ -2,7 +2,7 @@ import { IntelligenceItem, IntelligenceFetchResult } from './types';
 
 export async function fetchP4(): Promise<IntelligenceFetchResult> {
     try {
-        const res = await fetch('https://api.sr.se/api/v2/traffic/messages?format=json', {
+        const res = await fetch('https://api.sr.se/api/v2/traffic/messages?format=json&size=100', {
             cache: 'no-store'
         });
 
