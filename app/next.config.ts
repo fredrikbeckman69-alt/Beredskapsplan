9f5680a142702a1f7160db3b92ce8cc8cb168e2e
Fredrik Beckman <fredrik.beckman69@gmail.com>
2026-03-10 14:05:27 +0100

    Auto-commit: 2026-03-10 14:05:27

--- app/next.config.ts
+++ app/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: process.env.GITHUB_ACTIONS ? "export" : (process.env.DOCKER ? "standalone" : undefined),
-  basePath: "/Beredskapsplan",
   images: {
     unoptimized: true,
   },
